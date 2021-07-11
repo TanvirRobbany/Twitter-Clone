10b48b350b6f58482505ddb06f869112ec60167b
TanvirRobbany <trobbany@cisscom.com>
2021-07-11 18:48:15 +0600

    Follow API Developed

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -58,3 +58,38 @@ exports.signIn = async (req, res) => {
     const token = jwt.sign({id: user._id, name: user.user_name, auth: true}, process.env.jwt_secret);
     return res.status(200).json({token: token, msg: 'Successfully Logged In'})
 }
+
+exports.userList = async (req, res) => {
+    await USER.find({}).exec((err, users) => {
+        if(!users) {
+            res.status(200).json({msg: 'No Tweets Found'})
+        }
+        else if (err) {
+            res.status(400).json({msg: 'Bad Request'})
+        }
+        else {
+            res.status(200).json({users: users, msg: 'Tweets List Found'})
+        }
+    })
+}
+
+exports.userFollow =  (req, res) => {
+    const {follower_id, following_id} = req.body;
+    console.log(follower_id, follower_id)
+
+    USER.updateOne({_id: follower_id}, {$push: {following: following_id}}, async (err, follower) => {
+        if(err) {
+            res.status(400).json({msg: 'Bad Request'})
+        }
+        else {
+             USER.updateOne({_id: following_id}, {$push: {followers: follower_id}}, (err, following) => {
+                if(err) {
+                    res.status(400).json({msg: 'Bad Request'})
+                }
+                else {
+                    res.status(200).json({msg: 'Followed'})
+                }
+            })
+        }
+    })
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
-const {signUp, signIn} = require('../controller/authController');
+const {signUp, signIn, userList, userFollow} = require('../controller/authController');
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
+router.get('/users', userList);
+router.put('/user/follow', userFollow);
 
 module.exports = router;

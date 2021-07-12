360b44e50183ba1a3dc0c8c478d1e2d74657e8dc
TanvirRobbany <trobbany@cisscom.com>
2021-07-12 16:41:40 +0600

    Code Refactor

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -4,15 +4,15 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 exports.signUp = async (req, res) => {
-    const {name, email, password} = req.body;
+    const { name, email, password } = req.body;
 
     if (!name || !email || !password) {
-        res.status(400).json({msg: "All Fields Are Required!"})
+        res.status(400).json({ msg: "All Fields Are Required!" })
     }
 
-    const user = await USER.findOne({user_email: email});
+    const user = await USER.findOne({ user_email: email });
     if (user) {
-        res.status(409).json({msg: "User Alreary Exits!"})
+        res.status(409).json({ msg: "User Alreary Exits!" })
     }
 
     const salt = await bcrypt.genSalt(15);
@@ -26,69 +26,96 @@ exports.signUp = async (req, res) => {
 
     await newUser.save((err, user) => {
         if (err) {
-            res.status(400).json({msg: 'Bad Requests'})
+            res.status(400).json({ msg: 'Bad Requests' })
         }
         else {
-            res.status(200).json({user: user, msg: 'Registered Successfully'})
+            res.status(200).json({ user: user, msg: 'Registered Successfully' })
         }
     })
 }
 
 exports.signIn = async (req, res) => {
-    const {email, password} = req.body;
-    let  validatedPassword;
+    const { email, password } = req.body;
+    let validatedPassword;
 
     if (!email || !password) {
-        res.status(400).json({msg: 'All Fields Are Rquired'});
+        res.status(400).json({ msg: 'All Fields Are Rquired' });
     }
+    await USER.findOne({ user_email: email }).exec((err, user) => {
+        if (!user) {
+            res.status(404).json({ msg: "Not Registerd" })
+        }
+        else if (err) {
+            res.status(400).json({ msg: 'Bad Request' })
+        }
+        else {
+            bcrypt.compare(password, user.password, (err, isMatched) => {
+                if(!isMatched) {
+                    res.status(400).json({ msg: 'Invalid Credentials' })
+                }
+                else if (err) {
+                    res.status(400).json({ msg: 'Bad Request' })
+                }
+                else {
+                    const token = jwt.sign({ id: user._id, name: user.user_name, auth: true, user: user }, process.env.jwt_secret);
+                res.status(200).json({ token: token, msg: 'Successfully Logged In' })
+                }
+            });
+            // if (validatedPassword) {
+            //     const token = jwt.sign({ id: user._id, name: user.user_name, auth: true, user: user }, process.env.jwt_secret);
+            //     res.status(200).json({ token: token, msg: 'Successfully Logged In' })
+            // }
+        }
+    })
 
-    const user = await USER.findOne({user_email: email});
 
-    if (!user) {
-        res.status(404).json({msg: 'Not Registered'});
-    }
-    else {
-         validatedPassword = bcrypt.compare(password, user.password);
-    }
+    // if (!user) {
+    //     res.status(404).json({msg: 'Not Registered'});
+    // }
+    // else {
+    //      validatedPassword = bcrypt.compare(password, user.password);
+    // }
 
-    if (!validatedPassword) {
-        res.status(400).json({msg: 'Invalid Credentials'})
-    }
+    // if (!validatedPassword) {
+    //     res.status(400).json({msg: 'Invalid Credentials'})
+    // }
 
-    const token = jwt.sign({id: user._id, name: user.user_name, auth: true, user: user}, process.env.jwt_secret);
-    return res.status(200).json({token: token, msg: 'Successfully Logged In'})
+    // const token = jwt.sign({id: user._id, name: user.user_name, auth: true, user: user}, process.env.jwt_secret);
+    // if (user && validatedPassword){
+    //     return res.status(200).json({token: token, msg: 'Successfully Logged In'})
+    // }
 }
 
 exports.userList = async (req, res) => {
     await USER.find({}).exec((err, users) => {
-        if(!users) {
-            res.status(200).json({msg: 'No Tweets Found'})
+        if (!users) {
+            res.status(200).json({ msg: 'No Tweets Found' })
         }
         else if (err) {
-            res.status(400).json({msg: 'Bad Request'})
+            res.status(400).json({ msg: 'Bad Request' })
         }
         else {
-            res.status(200).json({users: users, msg: 'Tweets List Found'})
+            res.status(200).json({ users: users, msg: 'Tweets List Found' })
         }
     })
 }
 
-exports.userFollow =  (req, res) => {
-    const {follower_id, following_id} = req.body;
+exports.userFollow = (req, res) => {
+    const { follower_id, following_id } = req.body;
 
-    USER.updateOne({_id: follower_id}, {$push: {following: following_id}}, (err, follower) => {
-        if(err) {
-            res.status(400).json({msg: 'Bad Request'})
+    USER.updateOne({ _id: follower_id }, { $push: { following: following_id } }, (err, follower) => {
+        if (err) {
+            res.status(400).json({ msg: 'Bad Request' })
         }
         else {
-             USER.updateOne({_id: following_id}, {$push: {followers: follower_id}}, async (err, following) => {
-                if(err) {
-                    res.status(400).json({msg: 'Bad Request'})
+            USER.updateOne({ _id: following_id }, { $push: { followers: follower_id } }, async (err, following) => {
+                if (err) {
+                    res.status(400).json({ msg: 'Bad Request' })
                 }
                 else {
-                    const user =  await USER.findOne({_id: follower_id});
-                    const token = jwt.sign({name: user.user_name, user: user}, process.env.jwt_secret);
-                    res.status(200).json({token: token, msg: 'Followed'})
+                    const user = await USER.findOne({ _id: follower_id });
+                    const token = jwt.sign({ name: user.user_name, user: user }, process.env.jwt_secret);
+                    res.status(200).json({ token: token, msg: 'Followed' })
                 }
             })
         }
